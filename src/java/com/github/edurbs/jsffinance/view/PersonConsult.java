da5c7771d1332fe5593391b080b6859eb25add5c
Eduardo  Soares <edurbs@gmail.com>
2025-01-28 18:53:47 -0300

    add: person consult

---
+++ src/java/com/github/edurbs/jsffinance/view/PersonConsult.java
@@ -0,0 +1,21 @@
+package com.github.edurbs.jsffinance.view;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import lombok.Getter;
+
+@ManagedBean
+@Getter
+public class PersonConsult {
+    
+    private List<String> people = new ArrayList<>();
+    
+    @PostConstruct
+    public void init(){
+        for (int i = 0; i < 20; i++) {
+            people.add("");
+        }
+    }
+}

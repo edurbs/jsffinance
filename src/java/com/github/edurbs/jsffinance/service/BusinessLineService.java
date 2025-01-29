228b906e02d147ef520fcc8d2add219b18745f24
Eduardo  Soares <edurbs@gmail.com>
2025-01-29 20:52:00 -0300

    add business line service

---
+++ src/java/com/github/edurbs/jsffinance/service/BusinessLineService.java
@@ -0,0 +1,29 @@
+package com.github.edurbs.jsffinance.service;
+
+import com.github.edurbs.jsffinance.domain.BusinessLine;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class BusinessLineService {
+    
+    private static Map<Long, BusinessLine> businesslines = new HashMap<>();
+    
+    static {
+        businesslines.put(1L, new BusinessLine(1L, "Food"));
+        businesslines.put(2L, new BusinessLine(2L, "Education"));
+        businesslines.put(3L, new BusinessLine(3L, "Technology"));
+    }
+    
+    public List<BusinessLine> listAll(){
+        List<BusinessLine> list = new ArrayList<>();
+        list.addAll(businesslines.values());
+        return list;
+    }
+    
+    public BusinessLine findById(Long id){
+        return businesslines.get(id);
+    }
+
+}

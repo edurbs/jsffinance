078aa10faed23d24a1ba35bfc25fc7adf0e4932d
Eduardo  Soares <edurbs@gmail.com>
2025-01-29 15:46:15 -0300

    add: model to view

--- src/java/com/github/edurbs/jsffinance/view/PostRegistryBean.java
+++ src/java/com/github/edurbs/jsffinance/view/PostRegistryBean.java
@@ -1,11 +1,14 @@
 package com.github.edurbs.jsffinance.view;
 
 import com.github.edurbs.jsffinance.domain.Post;
+import com.github.edurbs.jsffinance.model.PostType;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
+import javax.faces.context.FacesContext;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -25,11 +28,18 @@ public class PostRegistryBean implements Serializable{
     
     public void add(){
         posts.add(post);
-        clean();
+        clean();        
+        String msg = "Posted with success!";
+        FacesContext.getCurrentInstance().addMessage(null, 
+                new FacesMessage(FacesMessage.SEVERITY_INFO, msg, msg));
     }
     
     public void clean(){
         post = new Post();
+    }
+    
+    public PostType[] getPostTypes(){
+        return PostType.values();
     }
     
 }

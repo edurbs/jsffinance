492591e3f08f460a3456a3e9d1c1d6b482ebc0b0
Eduardo  Soares <edurbs@gmail.com>
2025-01-29 19:39:37 -0300

    add: present of past validator

---
+++ src/java/com/github/edurbs/jsffinance/view/validator/PresentOrPastDateValidator.java
@@ -0,0 +1,31 @@
+package com.github.edurbs.jsffinance.view.validator;
+
+import com.sun.faces.util.MessageFactory;
+import java.time.LocalDate;
+import javax.faces.application.FacesMessage;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.validator.FacesValidator;
+import javax.faces.validator.Validator;
+import javax.faces.validator.ValidatorException;
+
+@FacesValidator("PresentOrPastDateValidator")
+public class PresentOrPastDateValidator implements Validator {
+
+    @Override
+    public void validate(FacesContext context, UIComponent component, Object value) throws ValidatorException {
+        if(value==null){
+            return;
+        }
+        LocalDate localDate = (LocalDate) value;
+        if(localDate.isAfter(LocalDate.now())){
+            Object label = MessageFactory.getLabel(context, component);
+            String errorDescription = label + " can't be in the future.";
+            FacesMessage messsage = new FacesMessage(FacesMessage.SEVERITY_ERROR,
+                    errorDescription, errorDescription);
+            throw new ValidatorException(messsage);
+        }
+        
+    }
+
+}

0273991b4c3c534777a7fa95b4b242ed9e34402b
edurbs <edurbs@gmail.com>
2025-02-04 15:04:52 -0300

    add download receipt option

--- src/main/java/com/github/edurbs/jsffinance/view/PostConsultBean.java
+++ src/main/java/com/github/edurbs/jsffinance/view/PostConsultBean.java
@@ -6,12 +6,20 @@ import com.github.edurbs.jsffinance.repository.PostRepository;
 import com.github.edurbs.jsffinance.service.PostUseCase;
 import com.github.edurbs.jsffinance.service.exception.BusinessException;
 import com.github.edurbs.jsffinance.view.util.FacesUtil;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 import javax.annotation.PostConstruct;
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
+
 import lombok.Getter;
 import lombok.Setter;
 
@@ -39,6 +47,29 @@ public class PostConsultBean implements Serializable {
         } catch (BusinessException e) {
             FacesUtil.addMessage(FacesMessage.SEVERITY_ERROR, e.getMessage());
         }        
+    }
+
+    public void downloadReceipt() throws IOException {
+        if(!selectedPost.isPaid() || selectedPost.getFile() == null){
+            FacesUtil.addMessage(FacesMessage.SEVERITY_ERROR, FacesUtil.getMessageI18n("entry_paid_cant_be_downloaded"));
+            return;
+        }
+        String filename = "receipt_" + selectedPost.getId() + ".pdf";
+        FacesContext facesContext = FacesContext.getCurrentInstance();
+        ExternalContext externalContext = facesContext.getExternalContext();
+        externalContext.responseReset();
+        externalContext.setResponseContentType("application/pdf");
+        externalContext.setResponseHeader("Content-Disposition", "attachment; filename=\"" + filename + "\"");
+        OutputStream outputStream =externalContext.getResponseOutputStream();
+        try (InputStream is = new ByteArrayInputStream(selectedPost.getFile())){
+            byte[] buffer = new byte[1024];
+            int bytesRead = -1;
+            while ((bytesRead = is.read(buffer)) != -1) {
+                outputStream.write(buffer, 0, bytesRead);
+            }
+            //outputStream.flush();
+            facesContext.responseComplete();
+        } 
     }
     
 }
